e85e6a991f91da839f8a0775a2fdbe3790a8b989
nessshon <shonness98@gmail.com>
2023-12-21 14:39:16 +0500

    Update version to 0.0.8.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiogram-tonconnect",
-    version="0.0.7",
+    version="0.0.8",
     author="nessshon",
     description="aiogram-tonconnect seamlessly integrates TonConnect into aiogram bots, "
                 "simplifying wallet connections and transactions with a ready UI. "
