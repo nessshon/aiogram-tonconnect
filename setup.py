973432051eeb556333fd4b5088616ac7540f00b2
nessshon <shonness98@gmail.com>
2024-01-20 06:32:48 +0500

    Update version to 0.12.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiogram-tonconnect",
-    version="0.12.0",
+    version="0.12.1",
     author="nessshon",
     description="TON Connect UI for aiogram bots.",
     long_description=long_description,
