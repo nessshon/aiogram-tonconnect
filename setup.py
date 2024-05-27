40515deb6b0d4d9955ff05d7f9bf97c2c6ef6f13
nessshon <shonness98@gmail.com>
2024-05-27 09:27:53 +0500

    Update version to `0.13.7`.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiogram-tonconnect",
-    version="0.13.6",
+    version="0.13.7",
     author="nessshon",
     description="TON Connect UI for aiogram bots.",
     long_description=long_description,
